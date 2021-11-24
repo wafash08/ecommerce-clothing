712c394736c38590ea1dfba01139a3f00ae4aee1
wafash08 <saefulhaqwafa@gmail.com>
2021-11-24 22:58:54 +0700

    break homepage into smaller component, finish styling new menu-item component

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,40 +1,10 @@
+import Directory from "../../components/directory/directory";
 import "./homepage.scss";
 
 export default function HomePage() {
   return (
     <div className='homepage'>
-      <div className='directory-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 }
