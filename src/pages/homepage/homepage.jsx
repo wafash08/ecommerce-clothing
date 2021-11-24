7a4186484e89f7f0e22959df8e530c3c0652e336
wafash08 <saefulhaqwafa@gmail.com>
2021-11-24 22:12:27 +0700

    created and styled homepage component

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,40 @@
+import "./homepage.scss";
+
+export default function HomePage() {
+  return (
+    <div className='homepage'>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
