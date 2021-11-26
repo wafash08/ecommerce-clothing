6f10307ab9b2e317995412476c25947ced917f04
wafash08 <saefulhaqwafa@gmail.com>
2021-11-26 16:30:41 +0700

    update menu-items to navigate with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
+
+import "./App.css";
+
+function HatsPage() {
+  return <h1>Hats Page</h1>;
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+        <Route
+          path='*'
+          element={
+            <main>
+              <p>There's nothing here!</p>
+              <Link to='/'>Back to home!</Link>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -9,7 +9,8 @@ export default class Directory extends React.Component {
         title: "hats",
         imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
         id: 1,
-        linkUrl: "shop/hats",
+        // linkUrl: "shop/hats",
+        linkUrl: "hats",
       },
       {
         title: "jackets",
@@ -43,8 +44,8 @@ export default class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...section }) => (
-          <MenuItem key={id} {...section} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
