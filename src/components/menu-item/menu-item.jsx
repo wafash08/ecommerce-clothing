c414d4da97877c031fb579ae866cb9f443b215b1
wafash08 <saefulhaqwafa@gmail.com>
2021-12-02 15:43:16 +0700

    improving accesibility on MenuItem component

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,11 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 import "./menu-item.scss";
 
-export default function MenuItem({ title, imageUrl, size, linkUrl }) {
+export default function MenuItem({ title, imageUrl, size = "", linkUrl }) {
   const navigate = useNavigate();
 
   return (
-    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
+    <button
+      className={`${size} menu-item`}
+      onClick={() => navigate(`${linkUrl}`)}
+    >
       <div
         style={{
           backgroundImage: `url(${imageUrl})`,
@@ -17,6 +20,6 @@ export default function MenuItem({ title, imageUrl, size, linkUrl }) {
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
       </div>
-    </div>
+    </button>
   );
 }
