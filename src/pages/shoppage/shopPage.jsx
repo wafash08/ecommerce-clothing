3834b2c89bde7f8f2d9cab6b053b367dd0bf26cd
wafash08 <saefulhaqwafa@gmail.com>
2021-12-27 23:19:38 +0700

    implemeting WithSpinner HOC on ShopPage component

--- src/pages/shoppage/shopPage.jsx
+++ src/pages/shoppage/shopPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes, useParams } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -10,18 +10,24 @@ import { updateCollections } from "../../redux/shop/shop.actions";
 
 import CollectionPage from "../collectionpage/collectionpage";
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import WithSpinner from "../../components/with-spinner/with-spinner";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 const ShopPage = ({ updateCollections }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
 
   useEffect(() => {
     const collectionRef = firestore.collection("collections");
-
     let unsubscribeFromSnapshot;
+
     unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       console.log(collectionsMap);
       updateCollections(collectionsMap);
+      setIsLoading(false);
     });
 
     return () => unsubscribeFromSnapshot();
@@ -30,10 +36,18 @@ const ShopPage = ({ updateCollections }) => {
   return (
     <div className='shop-page'>
       <Routes>
-        <Route path='/' element={<CollectionsOverview />} />
+        <Route
+          path='/'
+          element={<CollectionsOverviewWithSpinner isLoading={isLoading} />}
+        />
         <Route
           path=':collectionId'
-          element={<CollectionPage urlParams={params} />}
+          element={
+            <CollectionPageWithSpinner
+              urlParams={params}
+              isLoading={isLoading}
+            />
+          }
         />
       </Routes>
     </div>
